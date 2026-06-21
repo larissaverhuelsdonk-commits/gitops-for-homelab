d2ab21bcc3d306b1e77d325fb1d69d09d3cebfec
UGREEN USER <Ninarissa@debian.dedomain>
2026-06-21 21:49:40 +0200

    updates script to remove deleted service references.

--- postgres/dump_all_dbs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-mkdir -p backup
-
-timestamp=$(date +"%Y-%m-%d")
-
-docker compose exec -T db pg_dump -U "paperless"  > "~/backups/all-dbs-${timestamp}.sql"
