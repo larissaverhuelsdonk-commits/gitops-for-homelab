eef6ae7e351b7a578feab875bd918f08babc3044
UGREEN USER <Ninarissa@debian.dedomain>
2026-06-22 11:33:57 +0200

    Removes Restic, as it didn't seem super helpful during a real restoration procedure.

--- scripts/backup/full_backup.sh
+++ scripts/backup/full_backup.sh
@@ -17,5 +17,4 @@ run_step() {
 
 run_step "Paperless metadata DB dump" ./dump_paperless_metadata_db.sh
 run_step "Paperless document export" ./export_all_paperless_documents.sh
-run_step "Restic backup + retention" ./restic_backup_and_enforce_retention_policy.sh
 #run_step "Clear ~/backups/*" ./clear_temporary_backups.sh
--- scripts/backup/restic_backup_and_enforce_retention_policy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-(cd /volume1/my_apps/40-restic/ && docker compose -f backup.docker-compose.yml up)
-(cd /volume1/my_apps/40-restic/ && docker compose -f retention.docker-compose.yml up)
